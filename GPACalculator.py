a88ed5e793d27bc7429afd0e94a004af6889ede8
515Hacker515 <astryanmanvel15@gmail.com>
2024-03-02 09:24:00 -0800

    initial commit

---
+++ GPACalculator.py
@@ -0,0 +1,33 @@
+def count_GPA():
+
+    grades = input("Please write your all grades: ")
+    grades = grades.replace(" ", "")
+    count = 0
+    grade_dictionary = {'A': 4, 'B': 3, 'C': 2, 'D': 1, 'F': 0}
+
+    for i in grades:
+        i = i.upper()
+        if i in grade_dictionary.keys():
+            count += grade_dictionary[i]
+
+    return count/len(grades)
+
+# print(count_GPA())
+
+while True:
+
+    gpa = count_GPA()
+
+    if gpa <= 2.2:
+        print(gpa)
+        print('Your GPA is low, please do your work better to raise it up')
+    elif gpa >= 2.3 and gpa <= 3.4:
+        print(gpa)
+        print("Your GPA is normal, you are doing okay")
+    elif gpa >= 3.5:
+        print(gpa)
+        print("Your GPA is so good, you are one of the best students in the school, good job!")
+
+    finished_or_not = input('If you done with counting your GPA please write Done, if you want to continue write anything: ')
+    if finished_or_not.lower() == 'done':
+        break
